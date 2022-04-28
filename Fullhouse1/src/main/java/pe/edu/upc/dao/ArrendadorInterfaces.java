627a7bf9a4733fb0948ebf312f6221ce99bc44b2
Diego Porras <93242635+Diego2701@users.noreply.github.com>
2022-04-28 12:59:58 -0500

    Add files via upload

---
+++ Fullhouse1/src/main/java/pe/edu/upc/dao/ArrendadorInterfaces.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.intfaces;
+
+import java.util.List;
+
+import pe.edu.upc.entities.Arrendador;
+
+public interface ArrendadorInterfaces {
+
+	public void insert(Arrendador ar);
+
+	public List<Arrendador> list();
+
+}
