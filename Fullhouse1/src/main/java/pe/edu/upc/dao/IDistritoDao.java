eed70bac6b173f7fb40efce898b46ecab8672bd5
MiriamMamaniChullo <103612596+MiriamMamaniChullo@users.noreply.github.com>
2022-05-03 10:46:29 -0500

    Add files via upload

---
+++ Fullhouse1/src/main/java/pe/edu/upc/dao/IDistritoDao.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.daointerfaces;
+
+import java.util.List;
+
+import pe.edu.upc.entities.Distrito;
+
+public interface IDistritoDao {
+	public void insert(Distrito d);
+
+	public List<Distrito> list();
+
+	public void delete(int id);
+
+}
