91d1056cd2d4256fe3cdd6203ddbbe0df28ff896
MiriamMamaniChullo <103612596+MiriamMamaniChullo@users.noreply.github.com>
2022-05-03 10:52:41 -0500

    Add files via upload

---
+++ Fullhouse1/src/main/java/pe/edu/upc/service/IDistritoService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.serviceinterfaces;
+
+import java.util.List;
+
+import pe.edu.upc.entities.Distrito;
+
+public interface IDistritoService {
+	public void insert(Distrito d);
+
+	public List<Distrito> list();
+
+	public void delete(int id);
+}
